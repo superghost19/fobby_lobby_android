1549cc29bb0808d8c92f1379a89e581fb37e303d
Bo Sun <sunbo.boll@gmail.com>
2016-11-28 20:59:33 -0600

    added nearby button

--- app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/AgentListActivity.java
+++ app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/AgentListActivity.java
@@ -46,33 +46,28 @@ public class AgentListActivity extends AppCompatActivity {
         MySQLHelper db = new MySQLHelper(this);
         /** CRUD Operations **/
         // add Books
-        db.addAgent(new Agent("Zhou Agent", "500 W China Plaza", 40.1, 50.2, 5));
-        db.addAgent(new Agent("Zhang Agent", "2000 N State St", 40.2, 50.3, 3));
-        db.addAgent(new Agent("Buy New Home Real Estate", "1234 W Madison St", 40.3, 50.4, 4));
-        db.addAgent(new Agent("EasySell Real Estate", "567 W Monroe", 40.4, 50.5, 2));
+        if(db.getIds() == 0) {
+            db.addAgent(new Agent("Zhou Agent", "500 W China Plaza", 40.1, 50.2, 5));
+            db.addAgent(new Agent("Zhang Agent", "2000 N State St", 40.2, 50.3, 3));
+            db.addAgent(new Agent("Buy New Home Real Estate", "1234 W Madison St", 40.3, 50.4, 4));
+            db.addAgent(new Agent("EasySell Real Estate", "567 W Monroe", 40.4, 50.5, 2));
+        }
         // get all books
-        List<Agent> list = db.getAllAgents();
-        // update one book
-        int j = db.updateBook(list.get(2), "Liu Agent", "501 W China Plaza", 50.6, 40.5, 5);
-        // delete one book
-        db.deleteAgent(list.get(0));
+//        List<Agent> list = db.getAllAgents();
+//        // update one book
+//        int j = db.updateBook(list.get(2), "Liu Agent", "501 W China Plaza", 50.6, 40.5, 5);
+//        // delete one book
+//        db.deleteAgent(list.get(0));
         // get all books
         listItems = db.getAllAgents();
     }
-
-//    private class MyOnItemClickListener implements AdapterView.OnItemClickListener {
-//
-//        @Override
-//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//            Toast.makeText(getApplicationContext(), listItems.get(position).toString(), Toast.LENGTH_LONG).show();
-//        }
-//    }
 
     public class MyOnItemClickListener implements AdapterView.OnItemClickListener {
 
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-            String agentName = parent.getItemAtPosition(pos).toString();
+//            String agentName = parent.getItemAtPosition(pos).toString();
+            String agentName = listItems.get(pos).getName();
             Intent intent = new Intent(AgentListActivity.this, AgentDetailsActivity.class);
             intent.putExtra("EXTRA_AGENT_NAME", agentName);
             startActivity(intent);
--- app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/HomeActivity.java
+++ app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/HomeActivity.java
@@ -1,15 +1,30 @@
 package com.app.fobbylobby.fobbylobby_android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.app.fobbylobby.fobbylobby_android.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button nearbyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        nearbyButton = (Button) findViewById(R.id.nearbyAgentButton);
+        nearbyButton.setOnClickListener(new MyOnClickListener());
+    }
+
+    class MyOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HomeActivity.this, AgentListActivity.class);
+            startActivity(intent);
+        }
     }
 }

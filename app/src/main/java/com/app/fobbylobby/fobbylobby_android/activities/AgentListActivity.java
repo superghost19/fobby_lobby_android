f6ed0a0c1536585d6d9f8f30414c04dc26e4c1cf
Bo Sun <sunbo.boll@gmail.com>
2016-11-28 22:44:55 -0600

    added google map

--- app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/AgentListActivity.java
+++ app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/AgentListActivity.java
@@ -4,12 +4,13 @@ package com.app.fobbylobby.fobbylobby_android.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.app.fobbylobby.fobbylobby_android.R;
 import com.app.fobbylobby.fobbylobby_android.adapters.AgentListAdapter;
@@ -30,6 +31,9 @@ public class AgentListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agent_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
         makeChangesToDB();
         bindViews();
     }
@@ -40,6 +44,41 @@ public class AgentListActivity extends AppCompatActivity {
 
         listView.setAdapter(listAdapter);
         listView.setOnItemClickListener(new MyOnItemClickListener());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Toolbar tb = (Toolbar) findViewById(R.id.my_toolbar);
+        tb.inflateMenu(R.menu.agent_list_menu);
+        tb.setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        return onOptionsItemSelected(item);
+                    }
+                });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                break;
+
+            case R.id.action_map_view:
+                Intent intent = new Intent(this, AgentMapsActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                break;
+
+        }
+        return true;
     }
 
     private void makeChangesToDB() {

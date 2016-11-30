e4d8a6c3af07732ae89107f0000a5a9a0b813bb2
sliu102 <sliu102@hawk.iit.edu>
2016-11-29 22:02:23 -0600

    login page added

---
+++ app/src/main/java/com/app/fobbylobby/fobbylobby_android/activities/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.app.fobbylobby.fobbylobby_android.activities;
+
+/**
+ * Created by liushuxiao on 11/29/16.
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.app.fobbylobby.fobbylobby_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        loginBtn = (Button)findViewById(R.id.loginButton);
+
+
+    loginBtn.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            String userName = username.getText().toString();
+            String passWord = password.getText().toString();
+            if (userName.equals("abc") && passWord.equals("xyz")){
+
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }else{
+                Toast toast = Toast.makeText(getApplicationContext(),"Invalid username or password",Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        }
+    });
+    }
+}
